4e61c4fca3f1ed94d8b8ff1fd16eef52b44b0bba
Uzair Sammad <uzair.iu17@gmail.com>
2019-10-12 03:09:01 +0500

    created fetch api for calling data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Title from "./Components/Title/title";
+import Form from "./Components/Form/form";
+import Weather from "./Components/Weather/weather";
 
+const apiKey = "a7561bf85eeb8d4042a5a87ed820100e";
 class App extends Component {
+  getWeather = async e => {
+    e.preventDefault();
+
+    const apiCall = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`
+    );
+    const data = await apiCall.json();
+    console.log(data.name);
+    console.log(data.main.temp);
+    console.log(data.main.humidity);
+  };
+
   render() {
-    return <h1> It Works</h1>;
+    return (
+      <div>
+        <Title />
+        <Weather />
+        <Form getWeather={this.getWeather} />
+      </div>
+    );
   }
 }
 
--- src/Components/Form/form.js
+++ src/Components/Form/form.js
@@ -4,7 +4,7 @@ class Form extends Component {
   state = {};
   render() {
     return (
-      <form>
+      <form onSubmit={this.props.getWeather}>
         <input type="text" name="city" placeholder="City..." />
         <input type="text" name="country" placeholder="Country..." />
         <button type="submit">Get Weather</button>
