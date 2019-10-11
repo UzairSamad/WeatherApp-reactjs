9093d993af8996e1a976e192699bfc8202dad5e9
Uzair Sammad <uzair.iu17@gmail.com>
2019-10-12 02:28:29 +0500

    created title component

---
+++ src/Components/Title/title.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Title extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h2>Weather-App</h2>
+        <p1>Find out temprature,conditions and more</p1>
+      </div>
+    );
+  }
+}
+
+export default Title;
